2d124846897ab14629cbe3eeab720df801e78f22
Subhransu <subhranshumajhi2@gmail.com>
2023-09-05 00:46:30 +0530

    update user profile schema

--- models/profile.js
+++ models/profile.js
@@ -1,32 +1,29 @@
 const mongoose = require("mongoose");
 
-const profileSchema = new mongoose.Schema(
-  {
-    firstname: {
-      type: String,
-      required: true,
-    },
-    lastname: {
-      type: String,
-      required: true,
-    },
-    phone: {
-      type: Number,
-      required: true,
-    },
-    emergencycontact: {
-      type: Number,
-    },
-    dob: {
-      type: Date,
-      required: true,
-    },
-    gender: {
-      type: String,
-      required: true,
-    },
+const profileSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
   },
-  { timestamps: true }
-);
+  lastname: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  emergencycontact: {
+    type: Number,
+  },
+  dob: {
+    type: Date,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+});
 
 module.exports = profileSchema;
