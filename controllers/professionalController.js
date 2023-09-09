2224d1823244c86d56a054164e8d1b8127c40568
Subhransu <subhranshumajhi2@gmail.com>
2023-09-09 22:14:27 +0530

    add notify professional endpoint

--- controllers/professionalController.js
+++ controllers/professionalController.js
@@ -1,8 +1,11 @@
 const User = require("../models/user");
+const sendEmail = require("../utilities/sendEmail");
 
 const getAllProfessionals = async (req, res) => {
   try {
-    const professionals = await User.find({ isProfessional: true }).select("professional");
+    const professionals = await User.find({ isProfessional: true }).select(
+      "professional"
+    );
     res.status(200).json(professionals);
   } catch (err) {
     console.log(err);
@@ -13,10 +16,12 @@ const getAllProfessionals = async (req, res) => {
 const getDetails = async (req, res) => {
   const professionalId = req.params.id;
   try {
-    const professional = await User.findOne({"professional._id": professionalId}).select("professional");
+    const professional = await User.findOne({
+      "professional._id": professionalId,
+    }).select("professional");
 
-    if(!professional){
-      res.status(404).json({message: "Professional not found"});
+    if (!professional) {
+      res.status(404).json({ message: "Professional not found" });
     }
     res.status(200).json(professional);
   } catch (err) {
@@ -25,4 +30,47 @@ const getDetails = async (req, res) => {
   }
 };
 
-module.exports = { getAllProfessionals, getDetails };
+const notify = async (req, res) => {
+  const professionalId = req.params.id;
+  try {
+    const authHeader = await req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).json({ message: "Auth Header missing" });
+    }
+    const token = authHeader.split(" ")[1];
+    const clientInfo = await User.findOne({ user_token: token });
+
+    if (!clientInfo) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const professionalInfo = await User.findOne({"professional._id": professionalId});
+
+    if (!professionalInfo) {
+      return res.status(404).json({ message: "Professional not found" });
+    }
+
+    const email = professionalInfo.email;
+    const subject = `New Client Request: ${clientInfo.profile.firstname}`;
+    const content = `
+      <strong>Dear ${professionalInfo.username},</strong><br>
+      <p>You have received a new client request. Here are the details:</p>
+      <p>Name: <strong>${clientInfo.profile.firstname} ${clientInfo.profile.lastname}</strong></p>
+      <p>Email: <strong>${clientInfo.email}</strong></p>
+      <p>Contact No. : <strong>${clientInfo.profile.phone}</strong></p>
+      <p>Please <a href="${process.env.FRONTEND_URI}/login" target="_blank">log in</a> to your account for more information</p>
+      <p>Thank you for using Sukoon</p>
+      <br>
+      <strong>Best Regards,</strong><br>
+      <strong>Team Sukoon</strong>
+    `;
+
+    sendEmail(email, subject, content);
+    res.status(200).json({ message: "email sent" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+module.exports = { getAllProfessionals, getDetails, notify };
--- routes/professionalRoutes.js
+++ routes/professionalRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const {getAllProfessionals, getDetails} = require("../controllers/professionalController");
+const {getAllProfessionals, getDetails, notify} = require("../controllers/professionalController");
 
 router.route("/all").get(getAllProfessionals);
 router.route("/:id").get(getDetails);
+router.route("/:id/notify").get(notify);
 
 module.exports = router;
